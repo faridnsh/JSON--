17501f58998cf4060e1b187520f1df46c3155fd6
Farid Neshat <FaridN_SOAD@yahoo.com>
2012-08-20 16:01:12 +0800

    Add a nicer test/benchmark

--- test.js
+++ test.js
@@ -1,20 +1,22 @@
 var Parser = require('./index.js');
-var parser = new Parser(['rows', true, 'doc', '_id']);
 var npm = require('fs').readFileSync('./npm.json');
+var path = ['rows', true, 'doc', '_id'];
 
-console.time('JSON--');
-parser.write(npm);
-console.timeEnd('JSON--')
+path.reduce(function (path, item) {
+    path.push(item);
 
-console.time('toString')
+    var parser = new Parser(path);
+    var id = 'JSON--, path: ' + path;
+    console.time(id);
+    parser.write(npm);
+    console.timeEnd(id);
+    return path;
+}, []);
+
+console.time('toString');
 npm = npm.toString();
-console.timeEnd('toString')
+console.timeEnd('toString');
 
 console.time('JSON');
 JSON.parse(npm);
-console.timeEnd('JSON')
-
-// require('fs')
-//   .createReadStream('./npm.json')
-//   .on('data', parser.write.bind(parser))
-//   .on('end', console.log.bind(console, 'end'))
+console.timeEnd('JSON');
